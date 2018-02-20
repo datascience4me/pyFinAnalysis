80d46a8625a3e9e91ce971acee95c28f8d366fcd
mjovbg <35938823+mjovbg@users.noreply.github.com>
2018-02-20 23:52:02 +0100

    Add files via upload

--- 9_PyFinance/1_PF_Allocation.py
+++ 9_PyFinance/1_PF_Allocation.py
@@ -12,40 +12,51 @@ csco = quandl.get('WIKI/CSCO', start_date = start, end_date = end)
 ibm = quandl.get('WIKI/IBM', start_date = start, end_date = end)
 amzn = quandl.get('WIKI/AMZN', start_date = start, end_date = end)
 
-# Create Normalized (Cumulative) return:
+# print(aapl.iloc[0]['Adj. Close'])         # print rows with .iloc
+
+# #Create Normalized (Cumulative) return:
 for stock_df in (aapl,csco,ibm,amzn):
     stock_df['Norm Return'] = stock_df['Adj. Close']/stock_df.iloc[0]['Adj. Close']
+# print(stock_df.head())
+
 
 # Create portfolio allocations:
 for stock_df, allo in zip((aapl,csco,ibm,amzn),[0.3,0.2,0.4,0.1]):
     stock_df['Allocation'] = stock_df['Norm Return'] * allo
+# print(stock_df.head())
 
-# Invested Values ($1.000.000)
+# # Invested Values ($1.000.000) => multiply allocations with deposits
 for stock_df in (aapl,csco,ibm,amzn):
     stock_df['Position Value'] = stock_df['Allocation'] * 1000000
+# print(aapl.head())
 
+## Creating larger portfolio:
 all_pos_vals = [aapl['Position Value'], csco['Position Value'], ibm['Position Value'],amzn['Position Value']]
-portfolio_val = pd.concat(all_pos_vals, axis=1)
+portfolio_val = pd.concat(all_pos_vals, axis=1)       # concat previously made list
 portfolio_val.columns = ['AAPL Pos', 'CSCO Pos','IBM Pos', 'AMZN Pos']
-portfolio_val['Total Pos'] = portfolio_val.sum(axis = 1)
-# portfolio_val['Total Pos'].plot()
-# portfolio_val.drop('Total Pos', axis =1).plot()
-#
+portfolio_val['Total Pos'] = portfolio_val.sum(axis = 1)  # create total positions by summing by axes = 1 -> rows
+# print(portfolio_val.head())
+portfolio_val['Total Pos'].plot()       # plotting portfolio value over time
+portfolio_val.drop('Total Pos', axis =1).plot()   # check positions individually by dropping total pos column!
+
 # plt.show()
 
 
 # PART 2
 
-# Portfolio Stats
-portfolio_val['Daily Return'] = portfolio_val['Total Pos'].pct_change(1)
-portfolio_val['Daily Return'].mean()
-portfolio_val['Daily Return'].std()
-portfolio_val['Daily Return'].plot(kind = 'hist', bins = 100)
-cumulative_return = 100* (portfolio_val['Total Pos'][-1]/portfolio_val['Total Pos'][0]-1)   # total return
+# Portfolio Stats:
 
-# Sharpe Ratio
-sr = portfolio_val['Daily Return'].mean()/portfolio_val['Daily Return'].std
-sr = sr * (252**0.5)        # annualize = sqrt of 252
+portfolio_val['Daily Return'] = portfolio_val['Total Pos'].pct_change(1)
+# portfolio_val['Daily Return'].mean()  # get avg daily return column
+# portfolio_val['Daily Return'].std()   # get st dev column
+# portfolio_val['Daily Return'].plot(kind = 'hist', bins = 100)
+# cumulative_return = 100 * (portfolio_val['Total Pos'][-1]/portfolio_val['Total Pos'][0]-1)   # total return by grabbing last and first
+
+
+# # Sharpe Ratio:
+
+# sr = portfolio_val['Daily Return'].mean()/portfolio_val['Daily Return'].std
+# asr = sr * (252**0.5)        # annualized = sqrt of 252
 
 
 
--- 9_PyFinance/2_PF_Optimization.py
+++ 9_PyFinance/2_PF_Optimization.py
@@ -2,54 +2,88 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+'''
+how to get best portfolio, best sharpe ratio - which allocations???
+you can guess and try all possible combinations, i.e. use monte carlo analysis
+or you can optimize by using math -> use minimization  to create optimizer to minimize negative sharpe ratio
+use scipy built in algorithm
+'''
+
+# PART 1 - Monte Carlo Simulation:
+
 aapl = pd.read_csv('AAPL_CLOSE', index_col='Date', parse_dates=True)
 cisco = pd.read_csv('CISCO_CLOSE', index_col='Date', parse_dates=True)
 ibm = pd.read_csv('IBM_CLOSE', index_col='Date', parse_dates=True)
 amzn = pd.read_csv('AMZN_CLOSE', index_col='Date', parse_dates=True)
 
-stocks = pd.concat([aapl,cisco,ibm,amzn],axis=1)
+stocks = pd.concat([aapl,cisco,ibm,amzn],axis=1)    # concatenate all csv files together in one df
 stocks.columns = ['aapl', 'cisco', 'ibm', 'amzn']
+# print(type(stocks))
+# print(stocks.head())
 
 # print(stocks.pct_change(1).mean())      # avg daily return
 # print(stocks.pct_change(1).corr())      # correlation between daily returns
 
-# Log returns
+# # Log returns -> use it for de-trending (not arithmetic returns):
+
 log_ret = np.log(stocks/stocks.shift(1))
-# print(log_ret.cov()*252)
+# # print(log_ret.cov()*252)
+# log_ret.hist(bins = 100)
+# log_ret.cov()     # to get allocations
+# plt.show()
 
-# Setting weights
+# # Setting weights
 np.random.seed(101)
-weights = np.array(np.random.random(4))
-# print('Random Weights')
+# weights = np.array(np.random.random(4))
+# print('Random Weights:')
 # print(weights)
-
-# REPEATING (PART 2)
-num_ports = 1000
-all_weights = np.zeros((num_ports,len(stocks.columns)))
-ret_arr = np.zeros(num_ports)
-vol_arr = np.zeros(num_ports)
-sharpe_arr = np.zeros(num_ports)
-
-for ind in r
-
-# print('Rebalance')
-weights = weights/np.sum(weights)
+# print('Rebalanced Weights:')
+# weights = weights/np.sum(weights)       # rebalance them so the sum of them is equal to 1!
 # print(weights)
-
-# Expected Portfolio Return
-exp_ret = np.sum((log_ret.mean()*weights)*252)
-
-# Expected Variance (volatility)
-exp_vol = np.sqrt(np.dot(weights.T, np.dot(log_ret.cov()*252, weights)))   # complex look, but more code efficient
-
-# Sharpe Ratio
-# print('=' * 40)
-# print(' Sharpe Ratio')
-
-sr = exp_ret/exp_vol
+#
+# # Expected Return/Variance
+# exp_ret = np.sum((log_ret.mean()*weights)*252)
+# print('Expected Return:')
+# print(exp_ret)
+# exp_vol = np.sqrt(np.dot(weights.T,np.dot(log_ret.cov()*252,weights)))
+# print("Expected Volatility:")
+# print(exp_vol)
+#
+# sr = exp_ret/exp_vol
+# print('Sharpe Ratio:')
 # print(sr)
 
 # PART 2
+'''
+put previously made process into the for loop
+'''
+# Create variables to save results to be plotted later
+
+num_ports = 100        # no of portfolios
+all_weights = np.zeros((num_ports,len(stocks.columns)))
+ret_arr = np.zeros(num_ports)   # array that holds returns
+vol_arr = np.zeros(num_ports)   # array that holds vol.
+sharpe_arr = np.zeros(num_ports)    # array of all possible pf SR
+
+for ind in range(num_ports):
+    np.random.seed(101)
+    weights = np.array(np.random.random(4))
+    weights = weights / np.sum(weights)
+
+    # save weights:
+    all_weights[ind,:] = weights
+
+    # Exp return
+    ret_arr = np.sum((log_ret.mean() * weights) * 252)
+
+    # Exp vol
+    vol_arr = np.sqrt(np.dot(weights.T,np.dot(log_ret.cov()*252,weights)))
+
+    # Sharpe Ratio
+    sharpe_arr[ind] = ret_arr[ind]/vol_arr[ind]      # dividing every exp ret with its exp vol using ind from for loop
+
+
+
 
 
 
